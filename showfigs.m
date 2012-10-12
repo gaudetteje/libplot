60085c0449c0a1b92f363d1928ebcc1864a1d3ec
Jason Gaudette <jason.e.gaudette@navy.mil>
2012-10-12 15:28:41 -0400

    Updated showfigs function to accept arguments and return handles; updated eemd for efficiency

--- showfigs.m
+++ showfigs.m
@@ -1,8 +1,22 @@
-function showfigs
+function varargout = showfigs(varargin)
 % SHOWFIGS  brings all figure windows to front
+%
+% showfigs(FH) uses the handles listed in FH
+% FH = showfigs brings all figure windows to front and returns the handles
 
-FH = flipud(findobj('Type','figure')).';
+switch (nargin)
+    case 0
+        FH = flipud(findobj('Type','figure')).';
+    case 1
+        FH = varargin{1};
+    otherwise
+        error('Incorrect number of input parameters')
+end
 
 for f=FH
     figure(f)
+end
+
+if nargout > 0
+    varargout{1} = FH;
 end
